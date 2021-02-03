b55e951a1bc0a7229ba70e521e01e452f695b769
Mathew E Newton <elijah2895@hotmail.com>
2021-02-03 17:11:17 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -25,8 +25,7 @@ end
 
 def return_string_array(array)
   count = 0
-  while count < array.split do
-    puts array[count]
+  while count < array.length do
+    puts array[count].to_s
     count += 1
   end
-end
