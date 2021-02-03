1cca59adce5d6108e11f2b3292979ec89490510e
Mathew E Newton <elijah2895@hotmail.com>
2021-02-03 17:09:03 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,32 @@
 # Write your methods here
+def loop_message_five_times(message)
+  puts "#{message}"
+  puts "#{message}"
+  puts "#{message}"
+  puts "#{message}"
+  puts "#{message}"
+end
+
+def loop_message_n_times(message, limit)
+  count = 0
+  while count < limit do
+    puts message
+    count += 1
+  end
+end
+
+def output_array(array)
+  count = 0
+  while array[count] do
+    puts array[count]
+    count += 1
+  end
+end
+
+def return_string_array(array)
+  count = 0
+  while count < array.split do
+    puts array[count]
+    count += 1
+  end
+end
